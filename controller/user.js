c62aaff7cd52152f2b7ae59e2255d988a6448611
leeq1114 <ynliqiang811@gmail.com>
2020-07-18 19:27:25 +0900

    07_user login --server

--- controller/user.js
+++ controller/user.js
@@ -27,4 +27,51 @@ router.post('/registUser', async(ctx) => {
 });
 
 
+router.post('/loginUser', async(ctx) => {
+    // console.log('service is OK.');
+    // ctx.body = "OK!!!";
+
+    // 获取mongoose定义的User model
+    const User = mongoose.model('User');
+    // 接收post请求封装成User对象
+    let oldUser = new User(ctx.request.body);
+    let userName = oldUser.userName;
+    let password = oldUser.password;
+
+    // 
+    await User.findOne({userName : userName}).exec().then(async result=>{
+        if (result) {
+            let newUser = new User();
+            await newUser.comparePassword(password, result.password)
+            .then(isMatch=>{
+                // succeed
+                if (isMatch) {
+                    ctx.body = {
+                        code : 200,
+                        message : '登录成功',
+                        userInfo: {
+                            name: result.userName
+                        }
+                    }
+                } else {
+                    ctx.body = {
+                        code : 201,
+                        message : '登录失败'
+                    }
+                }
+            })
+        } else {
+            ctx.body = {
+                code : 201,
+                message : '用户名不存在'
+            }
+        }
+        
+    }).catch(err =>{
+        console.log(err);
+        
+    });
+});
+
+
 module.exports= router;
--- model/User.js
+++ model/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 
 const userSchema = new Schema({
@@ -9,6 +10,29 @@ const userSchema = new Schema({
     createDate : {type: Date, default: Date.now()}
 });
 
+// 数据保存之前加盐加密，save方法重写
+userSchema.pre('save', function(next){
+    // 随机生成salt，10是迭代次数
+    bcrypt.genSalt(10, (err, salt)=>{
+        if(err) return next();
+        bcrypt.hash(this.password, salt, (err, hash)=>{
+            if(err) return next(err);
+            this.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods = {
+    comparePassword: (oldPassword, password)=> {
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(oldPassword, password, (err, isMatch)=>{
+                if(!err) resolve(isMatch)
+                else reject(err);
+            });
+        });
+    }
+}
 // realese model
 mongoose.model('User', userSchema);
 
